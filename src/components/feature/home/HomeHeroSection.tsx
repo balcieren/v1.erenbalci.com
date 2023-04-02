7c9c912fd68ef9c33380e2af5305025cf44722a5
Eren BALCI <erenbalci3939@gmail.com>
2023-04-02 15:08:37 +0200

    ✏️ refactor: fix typo

--- src/components/feature/home/HomeHeroSection.tsx
+++ src/components/feature/home/HomeHeroSection.tsx
@@ -9,16 +9,16 @@ export const HomeHeroSection: FC = () => {
       id="hero"
       className="flex min-h-[80vh] items-center justify-center"
     >
-      <div className="flex max-w-full flex-col gap-6 text-center sm:max-w-3xl md:max-w-4xl">
+      <div className="flex flex-col max-w-full gap-6 text-center sm:max-w-3xl md:max-w-4xl">
         <h5 className="text-lg font-bold dark:text-white sm:text-3xl md:text-4xl">
           Hi, I am Eren 👋
         </h5>
         <h3 className="text-5xl font-extrabold dark:text-white sm:text-6xl md:text-7xl">
-          I try to be a good{" "}
+          I have been trying to be a good
           <span className="underline decoration-blue-400">developer</span> for
           +5 years.
         </h3>
-        <p className="md:text-x text-sm dark:text-zinc-200 sm:text-lg">
+        <p className="text-sm md:text-x dark:text-zinc-200 sm:text-lg">
           a{" "}
           <span className="font-bold dark:text-white">
             Full-Stack Developer
