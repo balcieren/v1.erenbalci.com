8c944fbca863dfcfb97990545a91997f736d5a58
Eren BALCI <erenbalci3939@gmail.com>
2023-05-06 00:45:16 +0200

    fix deployment

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,43 +1,43 @@
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { FC } from "react";
-import { RiGithubLine, RiInstagramLine, RiTwitterLine } from "react-icons/ri";
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { FC } from 'react';
+import { RiGithubLine, RiInstagramLine, RiTwitterLine } from 'react-icons/ri';
 
 const socialMedias = [
-  {
-    href: "https://www.instagram.com/eren__balci/",
-    icon: <RiInstagramLine />,
-  },
-  {
-    href: "https://twitter.com/eren__balci",
-    icon: <RiTwitterLine />,
-  },
-  {
-    href: "https://github.com/balcieren",
-    icon: <RiGithubLine />,
-  },
+	{
+		href: 'https://www.instagram.com/eren__balci/',
+		icon: <RiInstagramLine />,
+	},
+	{
+		href: 'https://twitter.com/eren__balci',
+		icon: <RiTwitterLine />,
+	},
+	{
+		href: 'https://github.com/balcieren',
+		icon: <RiGithubLine />,
+	},
 ];
 
 export const Footer: FC = () => {
-  return (
-    <footer className="flex flex-col-reverse items-center justify-between gap-y-4 border-t border-zinc-200 pt-8 pb-12 dark:border-zinc-600 sm:flex-row sm:gap-y-0">
-      <ul className="flex gap-6 text-zinc-600 dark:text-zinc-200">
-        {socialMedias.map(({ href, icon }, index) => (
-          <li key={index} className="cursor-pointer">
-            <Link href={href}>
-              <a target={"_blank"}>{icon}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="flex items-center gap-3">
-        <div className="flex rounded-full border border-zinc-600 p-1.5">
-          <EnvelopeIcon className="h-5 w-5 text-zinc-900 dark:text-white" />
-        </div>
-        <h3 className="text-sm font-bold dark:text-white">
-          erenbalci3939@gmail.com
-        </h3>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="flex flex-col-reverse items-center justify-between gap-y-4 border-t border-zinc-200 pb-12 pt-8 dark:border-zinc-600 sm:flex-row sm:gap-y-0">
+			<ul className="flex gap-6 text-zinc-600 dark:text-zinc-200">
+				{socialMedias.map(({ href, icon }, index) => (
+					<li key={index} className="cursor-pointer">
+						<Link href={href} target="_blank">
+							{icon}
+						</Link>
+					</li>
+				))}
+			</ul>
+			<div className="flex items-center gap-3">
+				<div className="flex rounded-full border border-zinc-600 p-1.5">
+					<EnvelopeIcon className="h-5 w-5 text-zinc-900 dark:text-white" />
+				</div>
+				<h3 className="text-sm font-bold dark:text-white">
+					erenbalci3939@gmail.com
+				</h3>
+			</div>
+		</footer>
+	);
 };
