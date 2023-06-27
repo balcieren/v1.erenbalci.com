719e064c8ff5480b65bca3ab7474cc07cdaf4dd2
Eren BALCI <erenbalci3939@gmail.com>
2023-06-27 19:58:57 +0200

    ♻️ refator: improve hero section and github project listing

--- src/components/feature/home/HomePage.tsx
+++ src/components/feature/home/HomePage.tsx
@@ -8,30 +8,37 @@ import { HomeQuestionSection } from "./HomeQuestionSection";
 import { HomeResumeSection } from "./HomeResumeSection";
 
 export const HomePage: NextPage<{ projects: Project[] }> = ({ projects }) => {
-  return (
-    <div className="flex flex-col gap-10">
-      <Head>
-        <title>Eren BALCI</title>
-      </Head>
-      <HomeHeroSection />
-      <HomeProjectSection projects={projects} />
-      <div className="flex flex-col gap-4 md:flex-row">
-        <HomeResumeSection className="w-full md:w-1/2" />
-        <HomeQuestionSection className="w-full md:w-1/2" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-10">
+            <Head>
+                <title>Eren BALCI</title>
+            </Head>
+            <HomeHeroSection />
+            <HomeProjectSection projects={projects} />
+            <div className="flex flex-col gap-4 md:flex-row">
+                <HomeResumeSection className="w-full md:w-1/2" />
+                <HomeQuestionSection className="w-full md:w-1/2" />
+            </div>
+        </div>
+    );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const projects = await axios.get<Project[]>(
-    "https://api.github.com/users/balcieren/repos?per_page=11"
-  );
+    const projects = await axios.get<Project[]>(
+        "https://api.github.com/users/balcieren/repos?per_page=50"
+    );
 
-  return {
-    props: { projects: projects.data.filter(({ fork }) => !fork) },
-    revalidate: 10,
-  };
+    return {
+        props: {
+            projects: projects?.data
+                .sort((a, b) => {
+                    return b.stargazers_count - a.stargazers_count;
+                })
+                .filter(({ fork }) => !fork)
+                .slice(0, 14),
+        },
+        revalidate: 10,
+    };
 };
 
 export default HomePage;
