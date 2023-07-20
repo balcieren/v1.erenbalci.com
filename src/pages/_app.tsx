17670c9b1fe38d286a57387cbeed32bc9ced5e1d
EREN BALCI <erenbalci3939@gmail.com>
2023-07-20 22:48:51 +0300

    ♻️ refactor: improve fasting uploading font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,23 @@ import { Footer, Header, Navbar } from "@components/layout";
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import { FC } from "react";
+import { Nunito } from "next/font/google";
 import "../styles/globals.css";
 
+const nunito = Nunito({ preload: false });
+
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="system">
-      <main>
-        <Header>
-          <Navbar />
-        </Header>
-        <Component {...pageProps} />
-        <Footer />
-      </main>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system">
+            <main className={nunito.className}>
+                <Header>
+                    <Navbar />
+                </Header>
+                <Component {...pageProps} />
+                <Footer />
+            </main>
+        </ThemeProvider>
+    );
 };
 
 export default App;
